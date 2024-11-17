52fc38c2ae025622160b68cb65dd5c74ad7e482b
Corrie <corriestoddard@gmail.com>
2024-11-17 07:51:41 +0000

    Put options container in options component

--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -7,43 +7,46 @@ import "../styles.css";
 
 export const Options = (props) => {
   return (
-    <div id="options">
-      <div className="directionLocation">
-        <Dimensions
-          isMoving={props.isMoving}
-          gridDimensions={props.gridDimensions}
-          treeProbability={props.treeProbability}
-          setGridDimensions={props.setGridDimensions}
-          setTreeProbability={props.setTreeProbability}
-        />
-        <Location
-          isMoving={props.isMoving}
-          dronePosition={props.dronePosition}
-          directions={props.directions}
-          droneDirection={props.droneDirection}
-          originalPosition={props.originalPosition}
-          setDroneDirection={props.setDroneDirection}
-          setDronePosition={props.setDronePosition}
-        />
-        <ControlCodes
-          windDisabled={props.windDisabled}
-          mode={props.mode}
-          isMoving={props.isMoving}
-          controlCodes={props.controlCodes}
-          setControlCodes={props.setControlCodes}
-          setMode={props.setMode}
-        />
-      </div>
+    <div id="options-container">
+      <h2>Grid Options</h2>
+      <div id="options">
+        <div className="directionLocation">
+          <Dimensions
+            isMoving={props.isMoving}
+            gridDimensions={props.gridDimensions}
+            treeProbability={props.treeProbability}
+            setGridDimensions={props.setGridDimensions}
+            setTreeProbability={props.setTreeProbability}
+          />
+          <Location
+            isMoving={props.isMoving}
+            dronePosition={props.dronePosition}
+            directions={props.directions}
+            droneDirection={props.droneDirection}
+            originalPosition={props.originalPosition}
+            setDroneDirection={props.setDroneDirection}
+            setDronePosition={props.setDronePosition}
+          />
+          <ControlCodes
+            windDisabled={props.windDisabled}
+            mode={props.mode}
+            isMoving={props.isMoving}
+            controlCodes={props.controlCodes}
+            setControlCodes={props.setControlCodes}
+            setMode={props.setMode}
+          />
+        </div>
 
-      <button
-        onClick={async () => {
-          props.setOriginalPosition(props.dronePosition);
-          await props.executeDelivery();
-        }}
-        disabled={props.isMoving}
-      >
-        <span>{props.mode === "d" ? "Delivery" : "Execute"}</span>
-      </button>
+        <button
+          onClick={async () => {
+            props.setOriginalPosition(props.dronePosition);
+            await props.executeDelivery();
+          }}
+          disabled={props.isMoving}
+        >
+          <span>{props.mode === "d" ? "Delivery" : "Execute"}</span>
+        </button>
+      </div>
     </div>
   );
 };
